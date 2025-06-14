a36e1c1031f88bf80aa182dcd4313ca26a5fee82
Javier Exeni <javierenriquevillagomezexeni98@gmail.com>
2025-06-14 11:16:15 -0400

    config: add error interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,15 @@
-import { ApplicationConfig } from "@angular/core";
-import { provideAnimationsAsync } from "@angular/platform-browser/animations/async";
-import { provideRouter } from "@angular/router";
+import { ApplicationConfig } from '@angular/core';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideRouter, withViewTransitions } from '@angular/router';
 
-import { routes } from "./app.routes";
-import { provideHttpClient } from "@angular/common/http";
+import { routes } from './app.routes';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { errorInterceptor } from './core/error.interceptor';
 
 export const appConfig: ApplicationConfig = {
-    providers: [provideRouter(routes), provideAnimationsAsync(), provideHttpClient()]
+  providers: [
+    provideRouter(routes, withViewTransitions()),
+    provideAnimationsAsync(),
+    provideHttpClient(withInterceptors([errorInterceptor])),
+  ],
 };
---
+++ src/app/core/error.interceptor.ts
@@ -0,0 +1,51 @@
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpInterceptorFn, HttpStatusCode } from '@angular/common/http';
+
+import { catchError, throwError } from 'rxjs';
+import { NotificationService } from './services/notification.service';
+
+export const errorInterceptor: HttpInterceptorFn = (req, next) => {
+  const notificationService = inject(NotificationService);
+  const router = inject(Router);
+
+  return next(req).pipe(
+    catchError((error: any) => {
+      switch (error.status) {
+        case HttpStatusCode.BadRequest:
+          notificationService.error(error.message, 10000);
+          break;
+        case HttpStatusCode.NotFound:
+          if (!router.url.includes('/login')) {
+            notificationService.error(
+              '¡Ups! Ruta no encontrada (Error 404)',
+              10000
+            );
+          }
+          break;
+        case HttpStatusCode.InternalServerError:
+          notificationService.error(
+            'Hubo un error desde el servidor, vuelva a intentarlo más tarde.',
+            10000
+          );
+          break;
+        case HttpStatusCode.Unauthorized:
+          notificationService.error(error.message, 10000);
+          break;
+        case HttpStatusCode.Conflict:
+          notificationService.error(
+            'Error en la inserción o actualización.',
+            10000
+          );
+          break;
+        default:
+          notificationService.error(
+            '¡Ups! Hubo un error desconocido, comuniquese con Javier Exeni.',
+            10000
+          );
+          break;
+      }
+      return throwError(() => error.error.message);
+    })
+  );
+};
